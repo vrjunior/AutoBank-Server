80218d0795ef2eeb36b54339fb62b6ae3b8a3334
Valmir Jr <valmirmassoni@gmail.com>
2016-11-30 15:31:24 -0200

    Base Authentication

--- src/main/java/us/guihouse/autobank/servlets/collaborator/JSessionAuthentication.java
+++ src/main/java/us/guihouse/autobank/servlets/collaborator/JSessionAuthentication.java
@@ -1,16 +1,25 @@
 package us.guihouse.autobank.servlets.collaborator;
 
+import us.guihouse.autobank.models.collaborator.Collaborator;
+import us.guihouse.autobank.repositories.CollaboratorRepository;
+import us.guihouse.autobank.repositories.ConnectionManager;
+
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.Connection;
 
 /**
  * Created by vrjunior on 25/11/16.
  */
 public class JSessionAuthentication extends javax.servlet.http.HttpServlet {
+    protected Collaborator collaborator;
+    protected ConnectionManager connectionManager;
+    protected Connection conn;
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
@@ -18,6 +27,20 @@ public class JSessionAuthentication extends javax.servlet.http.HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        connectionManager = new ConnectionManager();
+        HttpSession session = req.getSession();
+        Long id = (Long) session.getAttribute("collaboratorId");
+        if(id == null) {
+            resp.setStatus(401);
+            return;
+        }
+        this.conn = this.connectionManager.getConnection();
+        CollaboratorRepository collaboratorRepository = new CollaboratorRepository(this.conn);
+        try {
+            collaborator = collaboratorRepository.validateCollaboratorById(id);
+        } catch (CollaboratorRepository.NoAuthentication noAuthentication) {
+            resp.setStatus(401);
+            return;
+        }
     }
 }
--- web/main.jsp
+++ web/main.jsp
@@ -5,14 +5,15 @@
   Time: 18:39
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" extends="us.guihouse.autobank.servlets.collaborator.JSessionAuthentication" %>
+<%
+    super.doGet(request, response);
+%>
 <html>
 <head>
-    <title>Title</title>
+    <title>Dashboard - AutoBank</title>
 </head>
 <body>
-<%
-  out.print(session.getAttribute("collaboratorId"));
-%>
+hu3
 </body>
 </html>
